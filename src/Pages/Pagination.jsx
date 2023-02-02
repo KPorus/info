36a462e51de411e297edd0cce7e9229dd3b1ca7f
KPorus <designerfardin2021@gmail.com>
2023-02-02 17:31:33 +0600

    button ''

--- src/Pages/Pagination.jsx
+++ src/Pages/Pagination.jsx
@@ -9,13 +9,13 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
 
   return (
     <>
-      <ul className='pagination'>
+      <ul className='pagination flex justify-center'>
         {pageNumbers.map((number) => (
           <button
             type='button'
             key={number}
             onClick={() => paginate(number)}
-            className='page-link page px-8 py-3 font-semibold border rounded border-gray-600 lg:w-[8%]'>
+            className='page-link page sm:px-7 sm:py-3 px-6 py-1 font-semibold border rounded border-gray-600 w-[8%]'>
             {number}
           </button>
         ))}
